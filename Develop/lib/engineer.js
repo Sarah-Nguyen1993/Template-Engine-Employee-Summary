a3990c34418d02600c00e85bfc1507b70bd009ba
Dung Nguyen <dudu.nt107@gmail.com>
2020-10-19 18:45:53 -0500

    deleted definition of name, id, email. Don't need to redefine them

--- Develop/lib/engineer.js
+++ Develop/lib/engineer.js
@@ -2,9 +2,6 @@ const Employee  = require("../lib/Employee");
 class Engineer extends Employee {
     constructor(name, id, email,github){
         super (name, id, email)
-        this.name = name;
-        this.id = id;
-        this.email = email;
         this.github = github;
     };
     getRole(){
